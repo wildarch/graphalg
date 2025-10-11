04427961982045f2a33e1c4f32120a0cdb0d1297
Daan de Graaf <daandegraaf9@gmail.com>
2025-10-11 16:43:04 +0200

    Stub compiler.

--- cli/index.mjs
+++
@@ -1,8 +0,0 @@
-import {GraphAlg} from "codemirror-lang-graphalg"
-
-let tree = GraphAlg().language.parser.parse("func Test(a: Matrix<s, 1, int>) -> Vector<s, int> { return reduceRows(a); }")
-
-let cursor = tree.cursor()
-do {
-  console.log(`Node ${cursor.name} from ${cursor.from} to ${cursor.to}`)
-} while (cursor.next())
