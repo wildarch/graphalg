4db8ac33fb07e8d083f14975aa138c3873192c00
Ubuntu <daandegraaf9@gmail.com>
2025-11-02 22:13:38 +0000

    Start parsing matrix.

--- compiler/src/graphalg-exec.cpp
+++ compiler/src/graphalg-exec.cpp
@@ -1,3 +1,8 @@
+#include "graphalg/GraphAlgTypes.h"
+#include "mlir/IR/Attributes.h"
+#include "mlir/IR/BuiltinAttributes.h"
+#include "llvm/ADT/StringExtras.h"
+#include "llvm/ADT/StringRef.h"
 #include <cstdlib>
 #include <string>
 
@@ -14,6 +19,7 @@
 #include <mlir/IR/MLIRContext.h>
 #include <mlir/Parser/Parser.h>
 
+#include <graphalg/GraphAlgAttr.h>
 #include <graphalg/GraphAlgDialect.h>
 
 namespace cmd {
@@ -30,6 +36,35 @@ cl::list<std::string> args(cl::Positional, cl::desc("<arguments...>"),
                            cl::value_desc("input files"));
 
 } // namespace cmd
+
+static graphalg::MatrixAttr parseMatrix(llvm::Twine filename,
+                                        graphalg::MatrixType type) {
+  auto buffer = llvm::MemoryBuffer::getFileOrSTDIN(filename, true);
+  if (auto ec = buffer.getError()) {
+    llvm::WithColor::error() << "could not open argument file '" << filename
+                             << "': " << ec.message() << "\n";
+    return nullptr;
+  }
+
+  assert(type.getRows().isConcrete() && type.getCols().isConcrete());
+  auto rows = type.getRows().getConcreteDim();
+  auto cols = type.getCols().getConcreteDim();
+  llvm::SmallVector<mlir::Attribute> elems(rows * cols);
+
+  auto data = buffer->get()->getBuffer();
+  for (std::size_t i = 0; i < data.size();) {
+    auto lineEnd = data.find_first_of('\n', i);
+    if (lineEnd == std::string::npos) {
+      lineEnd = data.size();
+    }
+
+    auto line = data.substr(i, lineEnd);
+    llvm::SmallVector<llvm::StringRef, 3> parts;
+    llvm::SplitString(line, parts);
+    // TODO: use parts.
+    i = lineEnd + 1;
+  }
+}
 
 int main(int argc, char **argv) {
   llvm::cl::ParseCommandLineOptions(argc, argv, "Execute GraphAlg program\n");
@@ -54,13 +89,32 @@ int main(int argc, char **argv) {
   }
 
   // TODO: Load arguments
-  for (const auto &arg : cmd::args) {
-    auto buffer = llvm::MemoryBuffer::getFileOrSTDIN(arg, /*IsText=*/true);
+  llvm::SmallVector<graphalg::MatrixAttr> args;
+  // TODO: Different number of arguments.
+  for (const auto &[i, filename] : llvm::enumerate(cmd::args)) {
+    auto type = funcOp.getFunctionType().getInput(i);
+    /*
+    auto buffer = llvm::MemoryBuffer::getFileOrSTDIN(filename, true);
     if (auto ec = buffer.getError()) {
       llvm::WithColor::error() << "could not open argument file '" << arg
                                << "': " << ec.message() << "\n";
       return 1;
     }
+    */
+
+    auto matType = llvm::dyn_cast<graphalg::MatrixType>(type);
+    if (!matType) {
+      funcOp->emitOpError("parameter ")
+          << i << " is not of type " << graphalg::MatrixType::getMnemonic();
+      return 1;
+    }
+
+    auto parsed = parseMatrix(filename, matType);
+    if (!parsed) {
+      return 1;
+    }
+
+    args.push_back(parsed);
   }
 
   // TODO: Execute
