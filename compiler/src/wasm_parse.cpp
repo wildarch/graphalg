fcb5ff28bada5ba99601a48cc2b172ccebbd4b1b
Ubuntu <daandegraaf9@gmail.com>
2025-11-06 13:59:19 +0000

    Pass string back to JS.

--- compiler/src/wasm_parse.cpp
+++ compiler/src/wasm_parse.cpp
@@ -11,22 +11,53 @@
 
 #include "graphalg/parse/Parser.h"
 
-extern "C" {
+class Instance {
+private:
+  mlir::MLIRContext _ctx;
+  std::string _printed;
 
-void ga_parse(char *input) {
-  mlir::MLIRContext ctx;
-  ctx.getOrLoadDialect<mlir::BuiltinDialect>();
+public:
+  Instance();
 
+  bool tryParse(llvm::StringRef input);
+
+  const char *getPrinted();
+};
+
+Instance::Instance() { _ctx.getOrLoadDialect<mlir::BuiltinDialect>(); }
+
+bool Instance::tryParse(llvm::StringRef input) {
   llvm::StringRef filename = "<input>";
-  auto loc = mlir::FileLineColLoc::get(&ctx, filename,
+  auto loc = mlir::FileLineColLoc::get(&_ctx, filename,
                                        /*line=*/1, /*column=*/1);
   mlir::OwningOpRef<mlir::ModuleOp> moduleOp(
       mlir::ModuleOp::create(loc, filename));
 
   if (mlir::failed(graphalg::parse(input, *moduleOp))) {
     std::cerr << "failed to parse\n";
+    return false;
   }
 
-  moduleOp->print(llvm::outs());
+  _printed.clear();
+  llvm::raw_string_ostream os(_printed);
+  moduleOp->print(os);
+  return true;
 }
+
+const char *Instance::getPrinted() { return _printed.c_str(); }
+
+extern "C" {
+
+Instance *ga_new() { return new Instance(); }
+
+bool ga_try_parse(Instance *inst, char *input) { return inst->tryParse(input); }
+
+const char *ga_get_printed(Instance *inst) { return inst->getPrinted(); }
+
+void ga_free(Instance *inst) { delete inst; }
 }
+
+#ifndef __EMSCRIPTEN__
+// Dummy main when building for non-wasm platforms.
+int main(int argc, char **argv) { return 0; }
+#endif
--- compiler/src/wasm_parse.js
+++ compiler/src/wasm_parse.js
@@ -1,10 +1,18 @@
 var factory = require('/workspaces/graphalg/compiler/build-wasm/wasm_parse.js');
 
 factory().then((instance) => {
-    parse = instance.cwrap('ga_parse', null, ['string']);
-    parse(`
+    ga_new = instance.cwrap('ga_new', 'number', []);
+    ga_try_parse = instance.cwrap('ga_try_parse', 'number', ['number', 'string']);
+    ga_get_printed = instance.cwrap('ga_get_printed', 'number', ['number']);
+    ga_free = instance.cwrap('ga_free', null, ['number'])
+
+    inst = ga_new();
+    ga_try_parse(inst, `
         func Hello(a:int) -> int {
             return a;
         }
     `);
+    printed = instance.UTF8ToString(ga_get_printed(inst));
+    console.log(printed);
+    ga_free(inst);
 });
