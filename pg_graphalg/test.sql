c16a564bf9d59499ee2beced4e68703a28cf44cd
Ubuntu <daandegraaf9@gmail.com>
2025-11-22 21:15:33 +0000

    Port over tutorial impl.

--- pg_graphalg/test.sql
+++ pg_graphalg/test.sql
@@ -1,5 +1,16 @@
-CREATE OR REPLACE FUNCTION add_one(integer) RETURNS integer
-     AS '/workspaces/graphalg/pg_graphalg/build/src/libpg_graphalg.so', 'add_one'
-     LANGUAGE C STRICT;
+DROP FOREIGN TABLE IF EXISTS sequential_ints;
+DROP SERVER IF EXISTS graphalg_server;
+DROP FOREIGN DATA WRAPPER IF EXISTS graphalg_fdw;
+DROP FUNCTION IF EXISTS graphalg_fdw_handler;
 
-SELECT add_one(42);
+CREATE FUNCTION graphalg_fdw_handler()
+RETURNS fdw_handler
+AS '/workspaces/graphalg/pg_graphalg/build/src/libpg_graphalg.so'
+LANGUAGE C STRICT;
+
+CREATE FOREIGN DATA WRAPPER graphalg_fdw
+  HANDLER graphalg_fdw_handler;
+CREATE SERVER graphalg_server FOREIGN DATA WRAPPER graphalg_fdw;
+
+CREATE FOREIGN TABLE sequential_ints ( val int ) SERVER graphalg_server;
+SELECT * FROM sequential_ints;
