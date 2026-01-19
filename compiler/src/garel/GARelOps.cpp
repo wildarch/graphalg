087ef659a8bb1fd35834f55ed8002429840d7a03
Daan de Graaf <daandegraaf9@gmail.com>
2026-01-19 17:36:27 +0000

    verify ProjectOp.

--- compiler/src/garel/GARelOps.cpp
+++ compiler/src/garel/GARelOps.cpp
@@ -8,6 +8,47 @@
 
 namespace garel {
 
+mlir::LogicalResult ProjectOp::verifyRegions() {
+  if (getProjections().getNumArguments() != 1) {
+    return emitOpError("projections block should have exactly one argument");
+  }
+
+  auto blockArg = getProjections().getArgument(0);
+  auto blockType = llvm::dyn_cast<TupleType>(blockArg.getType());
+  if (!blockType) {
+    return emitOpError("projections block arg must be of type tuple");
+  }
+
+  if (getInput().getType().getColumns() != blockType.getColumns()) {
+    return emitOpError("projections block columns do not match input columns");
+  }
+
+  auto terminator = getProjections().front().getTerminator();
+  if (!terminator) {
+    return emitOpError("missing return from projections block");
+  }
+
+  auto returnOp = llvm::dyn_cast<ProjectReturnOp>(terminator);
+  if (!returnOp) {
+    return emitOpError("projections block not terminated by project.return");
+  }
+
+  if (returnOp.getProjections().size() != getType().getColumns().size()) {
+    return emitOpError("projections block returns a different number of "
+                       "values than specified in the projection return type");
+  }
+
+  for (const auto &[val, col] :
+       llvm::zip_equal(returnOp.getProjections(), getType().getColumns())) {
+    if (val.getType() != col.getType()) {
+      return emitOpError("projections block return types do not match the "
+                         "projection output column types");
+    }
+  }
+
+  return mlir::success();
+}
+
 mlir::LogicalResult ExtractOp::inferReturnTypes(
     mlir::MLIRContext *ctx, std::optional<mlir::Location> location,
     Adaptor adaptor, llvm::SmallVectorImpl<mlir::Type> &inferredReturnTypes) {
