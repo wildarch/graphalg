bde16924552a2ab372f991af9eb9613805e51e2a
Daan de Graaf <daandegraaf9@gmail.com>
2026-01-19 16:29:45 +0000

    Verifiers.

--- compiler/src/garel/GARelOps.cpp
+++ compiler/src/garel/GARelOps.cpp
@@ -1,7 +1,28 @@
-#include "garel/GARelOps.h"
+#include <llvm/ADT/STLExtras.h>
+
 #include "garel/GARelDialect.h"
+#include "garel/GARelOps.h"
 
 #define GET_OP_CLASSES
 #include "garel/GARelOps.cpp.inc"
 
-namespace garel {} // namespace garel
+namespace garel {
+
+mlir::LogicalResult ExtractOp::inferReturnTypes(
+    mlir::MLIRContext *ctx, std::optional<mlir::Location> location,
+    Adaptor adaptor, llvm::SmallVectorImpl<mlir::Type> &inferredReturnTypes) {
+  inferredReturnTypes.push_back(adaptor.getColumn().getType());
+  return mlir::success();
+}
+
+mlir::LogicalResult ExtractOp::verify() {
+  auto columns = getTuple().getType().getColumns();
+  if (!llvm::is_contained(columns, getColumn())) {
+    return emitOpError("column ")
+           << getColumn() << " not included in tuple " << getTuple().getType();
+  }
+
+  return mlir::success();
+}
+
+} // namespace garel
--- compiler/src/garel/GARelTypes.cpp
+++ compiler/src/garel/GARelTypes.cpp
@@ -1,3 +1,4 @@
+#include <llvm/ADT/ArrayRef.h>
 #include <llvm/ADT/TypeSwitch.h>
 #include <mlir/IR/Builders.h>
 #include <mlir/IR/DialectImplementation.h>
@@ -10,6 +11,40 @@
 #include "garel/GARelOpsTypes.cpp.inc"
 
 namespace garel {
+
+static mlir::LogicalResult
+verifyColumnsUnique(llvm::function_ref<mlir::InFlightDiagnostic()> emitError,
+                    llvm::ArrayRef<ColumnAttr> columns) {
+  // Columns must be unique
+  llvm::SmallDenseSet<ColumnAttr, 4> columnSet;
+  for (auto c : columns) {
+    auto [_, newlyAdded] = columnSet.insert(c);
+    if (!newlyAdded) {
+      return emitError() << "column " << c
+                         << " specified multiple times in the same column set";
+    }
+  }
+
+  return mlir::success();
+}
+
+mlir::LogicalResult
+RelationType::verify(llvm::function_ref<mlir::InFlightDiagnostic()> emitError,
+                     llvm::ArrayRef<ColumnAttr> columns) {
+  return verifyColumnsUnique(emitError, columns);
+}
+
+mlir::LogicalResult
+TupleType::verify(llvm::function_ref<mlir::InFlightDiagnostic()> emitError,
+                  llvm::ArrayRef<ColumnAttr> columns) {
+  return verifyColumnsUnique(emitError, columns);
+}
+
+bool isColumnType(mlir::Type t) {
+  // Allow i1, si64, f64, index
+  return t.isSignlessInteger(1) || t.isSignedInteger(64) || t.isF64() ||
+         t.isIndex();
+}
 
 // Need to define this here to avoid depending on IPRTypes in
 // IPRDialect and creating a cycle.
