afa3ffa89d9783b49711c717d4a5087b46f47fe2
Daan de Graaf <daandegraaf9@gmail.com>
2026-01-28 22:49:50 +0100

    Implemented row-wise max.

--- spec/formal/matlang.py
+++ spec/formal/matlang.py
@@ -47,10 +47,18 @@ def S_lte(M):
         v_emax = matmul(v, transp(emax(M)))
         return X + X_emax_v_v + v_emax
 
-    X = ffor(M, S_lte_inner)
+    # Output is square
+    out_shape = diag(one(M))
+    X = ffor(out_shape, S_lte_inner)
     # FIX not in original paper
     res = X - one(X) * transp(emax(X))
     return res
+
+def ident(M):
+    return diag(one(M))
+
+def S_lt(M):
+    return S_lte(M) - ident(M)
 
 def pickAny(M):
     def pickAny_row(v, X):
@@ -61,31 +69,50 @@ def pickAny(M):
             else: return y
         pickv = np.vectorize(pick)
         def pickAny_col(w, Y):
+            # D = 1(Y) * 1(Y).T * Y
             D = matmul(one(Y), matmul(transp(one(Y)), Y))
+            # P = diag(w) * B.T
             P = matmul(diag(w), transp(B))
             assert(Y.shape == D.shape)
             assert(Y.shape == P.shape)
-            res = pickv(Y, D, P)
-            return res
+            # apply[pick](Y, D, P)
+            return pickv(Y, D, P)
         row = ffor(transp(B), pickAny_col)
         return X + matmul(v, transp(row))
     return ffor(M, pickAny_row)
 
-A = np.array([
-    [0, 2, 3],
-    [4, 5, 6],
-    [0, 0, 9],
-    [0, 0, 0],
+def emin(M):
+    # NOTE: Different definition than the one in the for-ML paper
+    return transp(pickAny(transp(one(M))))
+
+def shift(M):
+    return pickAny(S_lt(M))
+
+def rotate(M):
+    return shift(M) + matmul(emax(M), transp(emin(M)))
+
+def max_element_vec(M):
+    R = rotate(M)
+    def max_element_inner(v, X):
+        # apply[max](M, R*X)
+        return np.maximum(M, matmul(R, X))
+    X = ffor(M, max_element_inner)
+    return matmul(transp(emax(M)), X)
+
+def max_element(M):
+    def max_row(v, X):
+        # One row of M
+        B = matmul(transp(v), M)
+        B_max = max_element_vec(transp(B))
+        B_max = matmul(v, B_max)
+        return X + B_max
+    return ffor(one(M), max_row)
+
+M = np.array([
+    [1, 2],
+    [4, 3],
+    [5, 6],
 ])
 
-print(pickAny(A))
+print(max_element(M))
 
-def ident(M):
-    return diag(one(M))
-
-M = np.zeros((10, 10))
-S_lt = S_lte(M) - ident(M)
-shift = pickAny(S_lt)
-print(shift)
-
-# TODO: emin
