0475061db13cd436e3861bb2299164a212d66244
Daan de Graaf <daandegraaf9@gmail.com>
2026-08-15 19:25:49 +0000

    Some algos to start with.

---
+++ algos/bc.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -ex
+
+cmake --build compiler/build --target graphalg-translate graphalg-opt graphalg-exec
+./compiler/build/tools/graphalg-translate --import-graphalg algos/bc.gr > /tmp/parsed.mlir
+./compiler/build/tools/graphalg-opt --graphalg-to-core-pipeline --graphalg-set-dimensions='func=BC args=9x9,9x9' /tmp/parsed.mlir > /tmp/exec.mlir
+./compiler/build/tools/graphalg-exec /tmp/exec.mlir BC algos/bc_toy.m algos/exchange.m
