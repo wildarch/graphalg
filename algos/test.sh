c461a4db46c1fca60db635396190fa2dbda68831
Daan de Graaf <daandegraaf9@gmail.com>
2026-08-15 20:51:19 +0000

    BC.

--- algos/test.sh
+++ algos/test.sh
@@ -3,5 +3,5 @@ set -ex
 
 cmake --build compiler/build --target graphalg-translate graphalg-opt graphalg-exec
 ./compiler/build/tools/graphalg-translate --import-graphalg algos/test.gr > /tmp/parsed.mlir
-./compiler/build/tools/graphalg-opt --graphalg-to-core-pipeline --graphalg-set-dimensions='func=BC args=9x9,9x9' /tmp/parsed.mlir > /tmp/exec.mlir
+./compiler/build/tools/graphalg-opt --graphalg-to-core-pipeline --graphalg-set-dimensions='func=BC args=5x5,5x5' /tmp/parsed.mlir > /tmp/exec.mlir
 ./compiler/build/tools/graphalg-exec /tmp/exec.mlir BC algos/bc_toy.m algos/exchange.m
