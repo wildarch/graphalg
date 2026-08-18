d412ec69647027e3e1cd38a5d96961b8e722bbb9
Daan de Graaf <daandegraaf9@gmail.com>
2026-08-18 19:41:21 +0000

    Bigger test matrix.

--- algos/kcore.sh
+++ algos/kcore.sh
@@ -3,5 +3,5 @@ set -ex
 
 cmake --build compiler/build --target graphalg-translate graphalg-opt graphalg-exec
 ./compiler/build/tools/graphalg-translate --import-graphalg algos/kcore.gr > /tmp/parsed.mlir
-./compiler/build/tools/graphalg-opt --graphalg-to-core-pipeline --graphalg-set-dimensions='func=KCore args=8x8,1x1' /tmp/parsed.mlir > /tmp/exec.mlir
-./compiler/build/tools/graphalg-exec /tmp/exec.mlir KCore algos/kcore_toy.m algos/kcore_k.m
+./compiler/build/tools/graphalg-opt --graphalg-to-core-pipeline --graphalg-set-dimensions='func=KCore args=16x16,1x1' /tmp/parsed.mlir > /tmp/exec.mlir
+./compiler/build/tools/graphalg-exec /tmp/exec.mlir KCore algos/kcore_toy.matrix algos/kcore_k.matrix
